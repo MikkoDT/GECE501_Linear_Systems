1522963eba9763d6c02583ed74f82847f420eb5f
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-04-14 13:32:46 +0800

    Update Exercise_05.py

--- Midterms/Midterms/Exercise_05.py
+++ Midterms/Midterms/Exercise_05.py
@@ -13,105 +13,126 @@ from madLib.madPlot import *
 from Data.variables import *
 
 
-
-fig5=Space3D(title="Figure 05",sz=(8,8)) 
-
-#define the figure
-
-fig5.ax=axes(projection='3d')
-
-fig5.Labels("The a plane Cut parallel to the lateral surface \
-generated a parabola.")
-fig5.ShowAxes(50)
+# defining the figure
+Fig5=Space3D(title="Figure 05",sz=(8,8));"Handler of Figure 5" 
+Fig5.ax=axes(projection='3d')
+Fig5.Labels("The disk cuts the cone at an angle parallel to the lateral surface. ")
+Fig5.ShowAxes(30)
 
 
-nbase=10
-nheight=40
-nTanHalfDelta=nbase/nheight
+"Values of the Cone"
+Base=8
+Height=25
+nTanHalfDelta=Base/Height
 delta=2*aTan(nTanHalfDelta)
 res=100
 
 
-"The cone equation"
-ec2=ec1.subs(u,z).subs(base,nbase).subs(height,nheight).simplify()
+"The cone equation in Cartesian Coordinate System"
+ec2=ec1.subs(u,z).subs(base,Base).subs(height,Height).simplify()
+
+"The cylindrical axes ."
+lnphi = linspace(0,2*npi,res);"One revolution."
+lnrho = linspace(0,Base,res);"From center to perimeter of the base."
+lnz1  = linspace(0,-Height,res);"Bottom cone from center to negative height."
 
 
-lnphi = linspace(0,2*npi,res)
-lnrho = linspace(0,nbase,res)
-lnz1  = linspace(0,-nheight,res)
-lnz2  = linspace(0,nheight,res)
+ncx=outer(cos(lnphi),lnrho); "Catersian meshgrid with cylindrical geometric equivalent cos(lnphi) and lnrho." 
+ncy=outer(sin(lnphi),lnrho); "Cartesian meshgrid with cylindrical geometric equivalent sin(lnphi) and lnrho."
+ncz1=outer(ones(res),lnz1);  "ncz1 is a Catersian meshgrid with cylindrical equivalent z for bottom cone."
 
-ncx=outer(cos(lnphi),lnrho)
-ncy=outer(sin(lnphi),lnrho)
-#ncz=outer(ones(res),lnz1)
-#fig5.ax.plot_surface(ncx,ncy,ncz,alpha=.2)
-ncz=outer(ones(res),lnz2)
-fig5.ax.plot_surface(ncx,ncy,ncz,alpha=.2)
+"Plotting of the  cone "
+Fig5.ax.plot_surface(ncx,ncy,ncz1,color=(1,0,0),alpha=.2)
+Fig5.ax.plot_surface(ncx,ncy,ncz1,color=(0,1,0),alpha=.2)
 
 
+"Plotting of the  cone "
+Fig5.ax.plot_surface(ncx,ncy,ncz1,color=(1,0,0),alpha=.2)
+Fig5.ax.plot_surface(ncx,ncy,ncz1,color=(0,1,0),alpha=.2)
 
 
+ncz=outer(ones(res),lnz1);"Lower cone"
+Fig5.ax.plot_surface(ncx,ncy,ncz,alpha=.2)
 
-"Equation of a circular plane perpendicular to cone height axis."
 
-nHCut=12
-nsvcTip=array([0,-nheight, 0])
-nsvcTail=array([0, 0, nbase])
-nsvc=nsvcTail-nsvcTip
-nloc=array([0,5,nHCut])
+nKDisk=-15
+nConeAngle=aTan(Base/Height)
+n1ConeAngle=nConeAngle*180/npi
 
-fig5.PlotVector(nloc,nsvc,color=(1,0,0),base=.5)
 
-ndisk=fig5.GenDisk(nbase*4)
-nrotdisk = fig5.QuaternionRotate( ndisk, array([0,0,1]), nsvc)
-nmovdisk= fig5.Move(nrotdisk,nloc)
-fig5.ax.plot_surface(nmovdisk[0],nmovdisk[1],nmovdisk[2],color=(0,1,1),alpha=.2)
+"let tilt angle be .25 less than "+str(nConeAngle)
+#nTiltAngle=npi/2-nConeAngle*1.2
+nDiskTilt=aTan(Base/Height)
+nDiskVectorTilt=npi/2-nDiskTilt
 
-linensvc=fig5.GenLine(nloc,-nsvc*2+nloc)
-fig5.ax.plot3D(linensvc[0],linensvc[1],linensvc[2],color=(0,0,0),
+
+nsVecTip=array([0,0,0])
+nsVecTail=array([Tan(nDiskVectorTilt)*nKDisk,0,nKDisk])
+nsVec=nsVecTip-nsVecTail
+nloc=array([0,0,nKDisk])
+
+Fig5.PlotVector(nloc,nsVec,color=(0,1,0),base=.5)
+nDisk=Fig5.GenDisk(Base*4)
+#fgr04.ax.plot_surface(ndisk[0],ndisk[1],ndisk[2],alpha=.2)
+nrotDisk = Fig5.QuaternionRotate( nDisk, nloc, nsVec)
+#fgr04.ax.plot_surface(nrotDisk[0],nrotDisk[1],nrotDisk[2],alpha=.2)
+nmovDisk= Fig5.Move(nrotDisk,nloc)
+Fig5.ax.plot_surface(nmovDisk[0],nmovDisk[1],nmovDisk[2],alpha=.2)
+
+
+linensVec=Fig5.GenLine(nloc,-nsVec+nloc)
+Fig5.ax.plot3D(linensVec[0],linensVec[1],linensVec[2],color=(0,0,0),
                 linestyle="dashed")
 
 
+e1ConeCyl=Eq(rho,base*(z)/height);"$\\rho$=f(z)"
 
-unsvc=fig5.UnitVector(nsvc)
-nuAcart=unsvc[0]
-nuBcart=unsvc[1]
-nuCcart=unsvc[2]
-nuKcart=dot(unsvc,nloc)
-plnCart=Eq(nuAcart*x+nuBcart*y+nuCcart*z,nuKcart)
-
-plndistorg=unsvc*nuKcart #plane distance to origin
-fig5.PlotVector([0,0,0], plndistorg,color=(0,0,1), base=1)
-
-sdy=solve(plnCart,y)[0]
+exc=Eq(x,base*z/height*Cos(phi));"x=f(z,$\\phi$)"
+eyc=Eq(y,base*z/height*Sin(phi));"x=f(z,$\\phi$)"
+e1ConeCar=Eq(x**2+y**2,(exc.rhs**2+eyc.rhs**2).simplify());"f(x,y)=f(z)"
+e2ConeCar=e1ConeCar.subs(base,Base).subs(height,Height)
+"Cartesian System cone equation"
+e2ConeCyl=Eq(rho**2,e2ConeCar.rhs);"Cylindrical System cone equation "
 
 
-ec4=Eq(x**2+y**2,base**2*z**2/height**2)
-ec5=ec4.subs(base,nbase).subs(height,nheight)
 
-sdy=solve(plnCart,y)[0]
-ec6=Eq(x**2,-(sdy**2).expand()+ec5.rhs)
+unsVec=Fig5.UnitVector(nsVec)
+nuAcar=unsVec[0]
+nuBcar=unsVec[1]
+nuCcar=unsVec[2]
+nuKcar=dot(unsVec,nloc)
+e1PlaneCar=Eq(nuAcar*x+nuBcar*y+nuCcar*z,nuKcar);"Cartesian system disk equation."
 
-sdx2=ec6.rhs
-sdx2roots=float(list(roots(sdx2).keys())[0])
+sdx=solve(e1PlaneCar,x)[0]
 
-niz=linspace(sdx2roots,nheight,res)
+sr2=e2ConeCar.rhs
 
+e3ConeCar=Eq(y**2,-sdx**2+sr2)
+sdy2=e3ConeCar.rhs
 
-ndy=[];ndxp=[];ndxn=[]
+sizroots=solve(Eq(sdy2,0),z)[0]
+
+niz=linspace(float(sizroots),-float(Height),res)
+
+        
+ndyn=[];ndyp=[];ndx=[];ndxn=[]
+
 
 for i in range(res):
-    ndy.append(sdy.subs(z,niz[i]))  
-    ndxp.append(Sqrt(sdx2.subs(z,niz[i])))
-    ndxn.append(-ndxp[i])
+    temp1=sdy2.subs(z,niz[i])
+    temp2=sdx.subs(z,niz[i])
+    if Ge(temp1,0):
+        ndyp.append(Sqrt(temp1))
+        ndyn.append(-Sqrt(temp1))
+    else:
+        ndyp.append(0)
+        ndyn.append(0)
+    ndx.append(temp2)  
+    #print(ndyp[i]**2+ndx[i]**2,sr2.subs(z,niz[i]))    
+
+Fig5.ax.plot(ndx,ndyp,niz)
+Fig5.ax.plot(ndx,ndyn,niz)
 
 
 
-
-fig5.ax.plot(ndxp,ndy,niz)
-fig5.ax.plot(ndxn,ndy,niz)
-
-
-
-savefig("Data/fig5.png")
-
+savefig("Data/Fig5.png")

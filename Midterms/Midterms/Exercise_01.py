cb8a14e0a4eb5fc7917a9831e4a68a0e72927ace
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-04-14 13:21:26 +0800

    update

--- Midterms/Midterms/Exercise_01.py
+++ Midterms/Midterms/Exercise_01.py
@@ -10,40 +10,37 @@ from madLib.madPlot import *
 from Data.variables import *
 
 
-
-Fig1=Space3D(title="Figure 1",sz=(8,8)) 
-
 #define the figure
-
+Fig1=Space3D(title="Figure 1",sz=(8,8)) 
 Fig1.ax=axes(projection='3d')
-
 Fig1.Labels("Inverted and Upright Cones")
 Fig1.ShowAxes(30)
 
-"Given value for cone equations"
-abase=30
-aheight=30
-aTanHalfDelta=abase/aheight
-delta=2*aTan(aTanHalfDelta)
+"Values for Cone Equations"
+Base=10
+Height=50
+TanHalfDelta=Base/Height
+delta=2*Tan(TanHalfDelta)
 res=100
 
+"The cone equation in the Cartesian Coordinates"
+ec2=ec1.subs(u,z).subs(base,Base).subs(height,Base).simplify()
+"Plotting the Cone"
 
-"The cone equation"
-ec2=ec1.subs(u,z).subs(base,abase).subs(height,aheight).simplify()
+phi = linspace(0,2*npi,res);"1 revolution"
+rho = linspace(0,Base,res);"From center to perimeter of the base."
+z1  = linspace(0,-Height,res);"The bottom cone from center to negative height."
+z2  = linspace(0,Height,res);"The top cone from center to positive height."
 
-"Plotting the cone"
-lnphi = linspace(0,2*npi,res)
-lnrho = linspace(0,abase,res)
-lnz1  = linspace(0,-aheight,res)
-lnz2  = linspace(0,aheight,res)
+cx=outer(cos(phi),rho);"Cartesian Meshgrid of geometic equivalent equal to cosine phi and rho"
+cy=outer(sin(phi),rho);"Cartesian Meshgrid of geometric equivalent equal to sine phi and rho"
+cz=outer(ones(res),z1);"Cartesian Meshgrid of bottom cyclindral equal to z"
 
-ncx=outer(cos(lnphi),lnrho)
-ncy=outer(sin(lnphi),lnrho)
-ncz=outer(ones(res),lnz1)
-Fig1.ax.plot_surface(ncx,ncy,ncz,alpha=.2)
-ncz=outer(ones(res),lnz2)
-Fig1.ax.plot_surface(ncx,ncy,ncz,alpha=.2)
+#Plotting of Figure in 3D space model
+Fig1.ax.plot_surface(cx,cy,cz,color = (0,0,1),alpha=.2)
+cz=outer(ones(res),z2)
+Fig1.ax.plot_surface(cx,cy,cz,color = (1,0,0),alpha=.2)
 
 
-
+#Saving the Figure
 savefig("Data/Fig1.png")

97c286bac4348de665d32e64f5f19f2d93612d2e
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-05-19 16:41:41 +0800

    update

--- Midterms_correction/Midterms/Exercise_02.py
+++ Midterms_correction/Midterms/Exercise_02.py
@@ -11,88 +11,80 @@ and cut the upright cone below the origin.Choose your own
 distance value below the origin.Formulate the disk equation."
 
 """
+from madLib.madVector import *
+from madLib.madPlot import *
+from Data.variables import *
 
-#define the figure
-Fig2=Space3D(title="Figure 02",sz=(8,8));"Disk cut on upright cone." 
-Fig2.ax=axes(projection='3d');"Plot axes handler."
+# Define the figure
+Fig2 = Space3D(title="Figure 02", sz=(8, 8)); "Disk cut on inverted cone."
+Fig2.ax = axes(projection='3d'); "Plot axes handler."
 
-Fig2.Labels("The upright cone with a diskcutting across upright cone")
+Fig2.Labels("The inverted cone with a disk cutting across inverted cone")
 Fig2.ShowAxes(30)
 
-"Values of the Cone"
-Base=10
-Height=25
-nTanHalfDelta=Base/Height
-delta=2*aTan(nTanHalfDelta)
-res=100
+# Values of the Cone
+Base = 10
+Height = 25
+nTanHalfDelta = Base / Height
+delta = 2 * aTan(nTanHalfDelta)
+res = 100
 
+# The cone equation in Cartesian Coordinate System
+# Assuming ec1 is defined elsewhere
+ec2 = ec1.subs(u, z).subs(base, Base).subs(height, Height).simplify()
 
-"The cone equation in Cartesian Coordinate System"
-ec2=ec1.subs(u,z).subs(base,Base).subs(height,Height).simplify()
+# The cylindrical axes
+lnphi = linspace(0, 2 * npi, res); "One revolution."
+lnrho = linspace(0, Base, res); "From center to perimeter of the base."
+lnz1 = linspace(0, Height, res); "Inverted cone from center to positive height."
 
-"The cylindrical axes ."
-lnphi = linspace(0,2*npi,res);"One revolution."
-lnrho = linspace(0,Base,res);"From center to perimeter of the base."
-lnz1  = linspace(0,-Height,res);"Bottom cone from center to negative height."
+ncx = outer(cos(lnphi), lnrho); "Cartesian meshgrid with cylindrical geometric equivalent cos(lnphi) and lnrho."
+ncy = outer(sin(lnphi), lnrho); "Cartesian meshgrid with cylindrical geometric equivalent sin(lnphi) and lnrho."
+ncz1 = outer(ones(res), lnz1); "ncz1 is a Cartesian meshgrid with cylindrical equivalent z for inverted cone."
 
+# Plotting of the inverted cone
+Fig2.ax.plot_surface(ncx, ncy, ncz1, color=(1, 0, 0), alpha=.2)
 
-ncx=outer(cos(lnphi),lnrho); "Catersian meshgrid with cylindrical geometric equivalent cos(lnphi) and lnrho." 
-ncy=outer(sin(lnphi),lnrho); "Cartesian meshgrid with cylindrical geometric equivalent sin(lnphi) and lnrho."
-ncz1=outer(ones(res),lnz1);  "ncz1 is a Catersian meshgrid with cylindrical equivalent z for bottom cone."
+# Equation of a cylindrical circular plane perpendicular to cone height axis.
+# Assuming cDisk is defined elsewhere
+cDisk = Eq(A * phi + B * rho + C * z, K)
+vDiskCoeff = Matrix([A, B, C]); "The [A, B, C] is a vector from origin perpendicular to the disk."
 
-"Plotting of the  cone "
-Fig2.ax.plot_surface(ncx,ncy,ncz1,color=(1,0,0),alpha=.2)
-Fig2.ax.plot_surface(ncx,ncy,ncz1,color=(0,1,0),alpha=.2)
+mVecDiskCoeff = Sqrt(A**2 + B**2 + C**2); "Magnitude of [A, B, C] vector"
+uVecDiskCoeff = vDiskCoeff / mVecDiskCoeff; "[A, B, C] unit vector"
+Diskxyz = Matrix([x, y, z]); "[x, y, z] is a vector from origin to any point in the disk."
 
+e1VecDisk = Eq(Matrix(dot(Diskxyz.transpose(), vDiskCoeff)), k, evaluate=False); "The dot product of [x, y, z] and [A, B, C] where θ is the angle between them."
+e2VecDisk = Eq(Matrix(dot(Diskxyz.transpose(), uVecDiskCoeff)), k / mVecDiskCoeff, evaluate=False); "The projection of [x, y, z] on [A, B, C] that gives the shortest distance from origin to the disk. The shortest distance is k/Mag([A, B, C])."
 
-"Equation of a cylindrical circular plane perpendicular to cone height axis."
-cDisk=Eq(A*phi+B*rho+C*z,K)
-vDiskCoeff=Matrix([A,B,C]);"The [A,B,C] is a vector from origin perpendicular to the disk."
+# The disk is perpendicular to z-axis and located at z=12. Hence the disk equation is expressed as follows.
+nADisk = 0; nBDisk = 0; nCDisk = 1; nKDisk = 12
 
-mVecDiskCoeff=Sqrt(A**2+B**2+C**2);"Magnitude of [A,B,C] vector"
-uVecDiskCoeff=vDiskCoeff/mVecDiskCoeff;"[A,B,C] unit vector"
-Diskxyz=Matrix([x,y,z]);"[x,y,z] is a vector from origin to any point in the disk."
-
-e1VecDisk=Eq(Matrix(dot(Diskxyz.transpose(),vDiskCoeff)),k,evaluate=False);"The dot produc of [x,y,z] and [A,B,C] where $\\theta$ is the angle between them."
-e2VecDisk=Eq(Matrix(dot(Diskxyz.transpose(),uVecDiskCoeff)),k/mVecDiskCoeff,evaluate=False);"The projection of [x,y,z] on [A,B,C] that gives the shortest distance from origin \
-to the disk. The shortest distance is k/Mag([A,B,C])."
-
-"The disk is perpendicular to z axis and located at z=-12. Hence the disk \
-equation is expressed as follows."
-nADisk=0;nBDisk=0;nCDisk=1;nKDisk=-12
-
-
-e1CylDisk=cDisk.subs(A,nADisk).subs(B,nBDisk).subs(C,nCDisk).subs(K,nKDisk)
+e1CylDisk = cDisk.subs(A, nADisk).subs(B, nBDisk).subs(C, nCDisk).subs(K, nKDisk)
 
 CaCyD[phi]
 CaCyD[rho]
 
+sVecDiskTip = array([0, 0, 0]); "Coordinate of the tip of nVecDiskCoeff"
+sVecDiskTail = array([0, 0, nKDisk]); "Coordinate of the tail of nVecDiskCoeff"
+sVecDisk = sVecDiskTip - sVecDiskTail; "[0, 0, nKDisk] vector"
+nloc = array([0, 0, nKDisk]); "Location of the disk."
+Fig2.PlotVector(nloc, sVecDisk, color=(1, 0, 0), base=.5)
 
-sVecDiskTip=array([0,0,0]);"Coordinate of the tip of nVecDiskCoeff"
-sVecDiskTail=array([0,0,nKDisk]);"Coordinate of the tail of nVecDiskCoeff"
-sVecDisk=sVecDiskTip-sVecDiskTail;"[0,0,nKDisk] vector"
-nloc=array([0,0,nKDisk]);"Location of the disk."
-Fig2.PlotVector(nloc,sVecDisk,color=(1,0,0),base=.5)
+# The plot algorithm is based on the cylindrical equation of disk.
+# Since the disk is a surface plot, the independent variables were chosen as φ and ρ as follows. The dependent variable is a constant. Hence z=12.
+nLphi = linspace(0, 2 * npi, res); "From zero to one complete revolution"
+nLrho = linspace(0, Base * 2, res); "From zero to radius of the disk"
+nLz = ones(res) * nKDisk; "Constant z=12 for all res data."
 
+mCarx = outer(cos(lnphi), nLrho); "Equivalent Cartesian x meshgrid of φ and ρ."
+mCary = outer(sin(lnphi), nLrho); "Equivalent Cartesian y meshgrid of φ and ρ."
+mCarz = outer(ones(res), nLz); "Equivalent Cartesian z meshgrid of z."
 
-"The plot algorithm is based on the cylindrical equation of disk. "
+Fig2.ax.plot_surface(mCarx, mCary, mCarz, alpha=.2)
 
-"Since the disk is a surface plot, the independent variables were chosen as $\\phi$ \
-and $\\rho$ as follows. The dependent varialbe is a constant. Hence z=-12."
+# Draw dashed line from point [0,0,nKDisk] to point [0,0,-nKDisk]
+linensVecDisk = Fig2.GenLine(nloc, -sVecDisk)
+Fig2.ax.plot3D(linensVecDisk[0], linensVecDisk[1], linensVecDisk[2], color=(0, 0, 0), linestyle="dashed")
 
-nLphi = linspace(0,2*npi,res);"From zero to one complete revolution"
-nLrho = linspace(0,Base*2,res);"Trom zero to  radius of the disk"
-nLz   = ones(res)*nKDisk;"Constant z=12 for all res data."
-
-mCarx=outer(cos(lnphi),nLrho);"Equivalent Cartesian x meshgrid of $\\phi$ and $\\rho$."
-mCary=outer(sin(lnphi),nLrho);"Equivalent Cartesian y meshgrid of $\\phi$ and $\\rho$."
-mCarz=outer(ones(res),nLz);"Equivalent Cartesian z meshgrid of z."
-
-Fig2.ax.plot_surface(mCarx,mCary,mCarz,alpha=.2)
-
-
-"Draw dashed line from point [0,0,nKDisk] to point [0,0,-nKDisk]"
-linensVecDisk=Fig2.GenLine(nloc,-sVecDisk)
-Fig2.ax.plot3D(linensVecDisk[0],linensVecDisk[1],linensVecDisk[2],color=(0,0,0),
-                linestyle="dashed")
 savefig("Data/Fig2.png")
--- Midterms_correction/Midterms/Exercise_06.py
+++ Midterms_correction/Midterms/Exercise_06.py
@@ -13,117 +13,100 @@ from madLib.madPlot import *
 from Data.variables import *
 
 # defining the figure
-Fig6=Space3D(title="Figure 06",sz=(8,8));"Handler of Figure 6" 
-Fig6.ax=axes(projection='3d')
-Fig6.Labels("The disk cuts upright and inverted cones at an angle. ")
+Fig6 = Space3D(title="Figure 06", sz=(8, 8)); "Handler of Figure 6"
+Fig6.ax = axes(projection='3d')
+Fig6.Labels("The disk cuts the upright and inverted cones at an angle.")
 Fig6.ShowAxes(30)
 
-"Values of the Cone"
-Base=8
-Height=25
-nTanHalfDelta=Base/Height
-delta=2*aTan(nTanHalfDelta)
-res=100
+# Values of the Cone
+Base = 8
+Height = 25
+nTanHalfDelta = Base / Height
+delta = 2 * aTan(nTanHalfDelta)
+res = 100
 
-"The cone equation in Cartesian Coordinate System"
-ec2=ec1.subs(u,z).subs(base,Base).subs(height,Height).simplify()
+# The cone equation in Cartesian Coordinate System
+ec2 = ec1.subs(u, z).subs(base, Base).subs(height, Height).simplify()
 
-"The cylindrical axes ."
-lnphi = linspace(0,2*npi,res);"One revolution."
-lnrho = linspace(0,Base,res);"From center to perimeter of the base."
-lnz1  = linspace(0,-Height,res);"Bottom cone from center to negative height."
+# The cylindrical axes
+lnphi = linspace(0, 2 * npi, res); "One revolution."
+lnrho = linspace(0, Base, res); "From center to perimeter of the base."
+lnz1 = linspace(0, -Height, res); "Bottom cone from center to negative height."
 
+ncx = outer(cos(lnphi), lnrho); "Cartesian meshgrid with cylindrical geometric equivalent cos(lnphi) and lnrho."
+ncy = outer(sin(lnphi), lnrho); "Cartesian meshgrid with cylindrical geometric equivalent sin(lnphi) and lnrho."
+ncz1 = outer(ones(res), lnz1); "ncz1 is a Cartesian meshgrid with cylindrical equivalent z for bottom cone."
 
-ncx=outer(cos(lnphi),lnrho); "Catersian meshgrid with cylindrical geometric equivalent cos(lnphi) and lnrho." 
-ncy=outer(sin(lnphi),lnrho); "Cartesian meshgrid with cylindrical geometric equivalent sin(lnphi) and lnrho."
-ncz1=outer(ones(res),lnz1);  "ncz1 is a Catersian meshgrid with cylindrical equivalent z for bottom cone."
+# Plotting of the inverted cone
+Fig6.ax.plot_surface(ncx, ncy, ncz1, alpha=.2)
 
-ncz=outer(ones(res),lnz1);"Lower cone"
-Fig6.ax.plot_surface(ncx,ncy,ncz,alpha=.2)
+# Tilt angle to create hyperbolic intersection
+nConeAngle = aTan(Base / Height)
+nDiskTilt = nConeAngle + 0.1  # Slightly greater than the cone angle for hyperbolic intersection
+nDiskVectorTilt = npi / 2 - nDiskTilt
 
+nsVecTip = array([Height, 0, -Height])
+nsVecTail = array([0, 0, -Height])
+nsVec = nsVecTip - nsVecTail
+nloc = array([0, 0, -Height])
 
-nKDisk=-15
-nConeAngle=aTan(Base/Height)
-n1ConeAngle=nConeAngle*180/npi
-"let tilt angle be .25 less than "+str(nConeAngle)
+Fig6.PlotVector([5, 0, -Height], nsVec, color=(1, 0, 0), base=.5)
+nDisk = Fig6.GenDisk(Base * 4)
+nrotDisk = Fig6.QuaternionRotate(nDisk, nloc, nsVec)
+nmovDisk = Fig6.Move(nrotDisk, [5, 0, -Height])
+Fig6.ax.plot_surface(nmovDisk[0], nmovDisk[1], nmovDisk[2], alpha=.2)
 
-nDiskTilt=nConeAngle
-nDiskVectorTilt=npi/2-nDiskTilt
+linensVec = Fig6.GenLine([5, 0, -Height], -nsVec + nloc)
+Fig6.ax.plot3D(linensVec[0], linensVec[1], linensVec[2], color=(0, 0, 0), linestyle="dashed")
 
-
-nsVecTip=array([Height,0,nKDisk])
-nsVecTail=array([0,0,nKDisk])
-nsVec=nsVecTip-nsVecTail
-nloc=array([0,0,nKDisk])
-
-Fig6.PlotVector([5,0,nKDisk],nsVec,color=(1,0,0),base=.5)
-nDisk=Fig6.GenDisk(Base*4)
-nrotDisk = Fig6.QuaternionRotate( nDisk, nloc, nsVec)
-nmovDisk= Fig6.Move(nrotDisk,[5,0,nKDisk])
-Fig6.ax.plot_surface(nmovDisk[0],nmovDisk[1],nmovDisk[2],alpha=.2)
-
-
-linensVec=Fig6.GenLine([5,0,nKDisk],-nsVec+nloc)
-Fig6.ax.plot3D(linensVec[0],linensVec[1],linensVec[2],color=(0,0,0),
-                linestyle="dashed")
-
-
-
-e1ConeCyl=Eq(rho,base*(z)/height);"$\\rho$=f(z)"
-
-exc=Eq(x,base*z/height*Cos(phi));"x=f(z,$\\phi$)"
-eyc=Eq(y,base*z/height*Sin(phi));"x=f(z,$\\phi$)"
-e1ConeCar=Eq(x**2+y**2,(exc.rhs**2+eyc.rhs**2).simplify());"f(x,y)=f(z)"
-e2ConeCar=e1ConeCar.subs(base,Base).subs(height,Height)
-
+e1ConeCyl = Eq(rho, base * (z) / height); "$\\rho$=f(z)"
+exc = Eq(x, base * z / height * Cos(phi)); "x=f(z,$\\phi$)"
+eyc = Eq(y, base * z / height * Sin(phi)); "x=f(z,$\\phi$)"
+e1ConeCar = Eq(x**2 + y**2, (exc.rhs**2 + eyc.rhs**2).simplify()); "f(x,y)=f(z)"
+e2ConeCar = e1ConeCar.subs(base, Base).subs(height, Height)
 
 "Cartesian System cone equation"
-e2ConeCyl=Eq(rho**2,e2ConeCar.rhs)
+e2ConeCyl = Eq(rho**2, e2ConeCar.rhs)
 
-
-
-unsVec=Fig6.UnitVector(nsVec)
-nuAcar=unsVec[0]
-nuBcar=unsVec[1]
-nuCcar=unsVec[2]
-nuKcar=dot(unsVec,[5,0,nKDisk])
+unsVec = Fig6.UnitVector(nsVec)
+nuAcar = unsVec[0]
+nuBcar = unsVec[1]
+nuCcar = unsVec[2]
+nuKcar = dot(unsVec, [5, 0, -Height])
 
 "Cartesian system disk equation."
-e1PlaneCar=Eq(nuAcar*x+nuBcar*y+nuCcar*z,nuKcar)
+e1PlaneCar = Eq(nuAcar * x + nuBcar * y + nuCcar * z, nuKcar)
 
-sdx=solve(e1PlaneCar,x)[0]
+sdx = solve(e1PlaneCar, x)[0]
+sr2 = e2ConeCar.rhs
+e3ConeCar = Eq(y**2, -sdx**2 + sr2)
+sdy2 = e3ConeCar.rhs
+sizroots = solve(Eq(sdy2, 0), z)[0]
+niz = linspace(float(sizroots), -float(Height), res)
 
-sr2=e2ConeCar.rhs
-
-e3ConeCar=Eq(y**2,-sdx**2+sr2)
-sdy2=e3ConeCar.rhs
-
-sizroots=solve(Eq(sdy2,0),z)[0]
-
-niz=linspace(float(sizroots),-float(Height),res)
-
-        
-ndyn=[];ndyp=[];ndx=[];ndxn=[]
-
+ndyn = []
+ndyp = []
+ndx = []
+ndxn = []
 
 for i in range(res):
-    temp1=sdy2.subs(z,niz[i])
-    temp2=sdx.subs(z,niz[i])
-    if Ge(temp1,0):
+    temp1 = sdy2.subs(z, niz[i])
+    temp2 = sdx.subs(z, niz[i])
+    if Ge(temp1, 0):
         ndyp.append(Sqrt(temp1))
         ndyn.append(-Sqrt(temp1))
     else:
         ndyp.append(0)
         ndyn.append(0)
-    ndx.append(temp2)  
-    #print(ndyp[i]**2+ndx[i]**2,sr2.subs(z,niz[i]))    
-Fig6.ax.plot(ndx,ndyp,niz)
-Fig6.ax.plot(ndx,ndyn,niz)
+    ndx.append(temp2)
 
-niz=linspace(-float(sizroots),float(Height),res)     
-ndyn=[];ndyp=[];ndx=[];ndxn=[]
+Fig6.ax.plot(ndx, ndyp, niz)
+Fig6.ax.plot(ndx, ndyn, niz)
 
-
-
+niz = linspace(-float(sizroots), float(Height), res)
+ndyn = []
+ndyp = []
+ndx = []
+ndxn = []
 
 savefig("Data/Fig6.png")
